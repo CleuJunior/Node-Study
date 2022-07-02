3b6703039cc0b5dbc4f28d62506650518fca8a0f
Cleonildo Jr <cleu.junior@gmail.com>
2022-07-01 22:41:14 -0300

    Events

---
+++ events-js/events.js
@@ -0,0 +1,10 @@
+import { EventEmitter } from 'events';
+
+const eventEmitter = new EventEmitter();
+
+
+eventEmitter.on("testeEvent", obj => {
+    console.log(obj);
+});
+
+eventEmitter.emit("testeEvent", "abc");
